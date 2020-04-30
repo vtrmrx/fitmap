a8664f07fb2a8d614d4be330f8e9cc5afcf9e3ad
Vítor Marques de Faria <vtrmrx@gmail.com>
2020-04-30 01:40:11 -0300

    installs and tests react

---
+++ src/ui.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import './ui.css'
+
+class App extends React.Component {
+  render() {
+    return <div>
+      <h1>Fitmap</h1>
+    </div>
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById('react-page'))
